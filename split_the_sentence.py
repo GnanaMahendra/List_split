8605bb04c33d7e0bc07cd4b11e1a1e8076c800d7
gnanamahendra28 <gnanamahendra28@gmail.com>
2024-12-23 17:15:31 +0530

    second commit

--- split_the_sentence.py
+++ split_the_sentence.py
@@ -1,3 +1,14 @@
+'''give A sentence s. The words in the sentence s are seperated by a spaces
+write a program that prints a list contains a words in the sentence.
+example:
+
+input:
+    s = "Knowledge is the power"
+output:
+    ['Knowledge','is','the','power']
+'''
+
+
 s= input()
 a = s.split()
 print(a)

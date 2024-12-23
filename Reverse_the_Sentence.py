3569f4f324310a32aa4a5597df7d640881f40b45
gnanamahendra28 <gnanamahendra28@gmail.com>
2024-12-23 19:40:48 +0530

    first commit

---
+++ Reverse_the_Sentence.py
@@ -0,0 +1,21 @@
+'''
+Given a sentence s, Write a program to print the reverse of th sentence.
+
+sample(i/o)
+
+input:
+    the train came on time
+output:
+    emit no emac niart eht
+
+
+sample(i/o)
+
+input:
+    I am learning Python
+output:
+    nohtyP gninrael ma I
+'''
+
+word = input()
+print(word[::-1])

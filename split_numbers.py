6c093ae7f5eba91a85c5784fded5cd9d755cba02
gnanamahendra28 <gnanamahendra28@gmail.com>
2024-12-23 17:46:05 +0530

    first commit split_numbers

---
+++ split_numbers.py
@@ -0,0 +1,19 @@
+'''
+Give space-separated numbers,Write a program to printa list containing the given numbers.
+
+example:
+input:
+    10 20 30 40 50
+output:
+    [10, 20, 30, 40, 50]
+'''
+
+number = input()
+nums = number.split() #remove the space in the number ver
+ 
+list_output = []
+
+for i in nums:
+    num = int(i)
+    list_output += [num]
+print(list_output)

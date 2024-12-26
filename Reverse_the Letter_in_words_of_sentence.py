584fc0e87963d70c8a31f7265131c0cfe55bb182
gnanamahendra28 <gnanamahendra28@gmail.com>
2024-12-26 16:43:42 +0530

    second commit on maendra branch

--- Reverse_the Letter_in_words_of_sentence.py
+++ Reverse_the Letter_in_words_of_sentence.py
@@ -3,23 +3,37 @@ Given a sentence, Write a program to reverse yhr letters in words of the sentenc
 
 sample(i/o)
 input:
+
     The cat is on the mat.
+
 output:
+
     ehT tac si no eht .tam
 
 sample(i/o)
+
 input:
+
     Have a nice weekend.
+
 output:
+
     evaH a ecin .dnekeew
 
 '''
+# Source Code
+
+
 word = input()
 
 word = word.split()
 
 words = []
+
 for i in word:
-    words += [i[::-1]]
-words = ",".join(words)
+
+    words += [ i [ : : -1 ] ]
+
+words = ",".join( words )
+
 print(words)
